4ea0bea0d56c5e08260214b9faf60a55f125257b
leommxj <leommxj@gmail.com>
2024-08-28 22:58:34 +0800

    port to gnuradio 3.10

---
+++ python/adsbout/__init__.py
@@ -0,0 +1,27 @@
+#
+# Copyright 2008,2009 Free Software Foundation, Inc.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+# The presence of this file turns this directory into a Python package
+
+'''
+This is the GNU Radio ADSBOUT module. Place your Python package
+description here (python/__init__.py).
+'''
+import os
+
+# import pybind11 generated symbols into the adsbout namespace
+try:
+    # this might fail if the module is python-only
+    from .adsbout_python import *
+except ModuleNotFoundError:
+    pass
+
+# import any pure python here
+#
+from .adsb_position_source import adsb_position_source
+from .adsb_Concentrator import adsb_Concentrator
+from .adsb_ident_source import adsb_ident_source
+
--- python/adsbout/adsb_position_source.py
+++ python/adsbout/adsb_position_source.py
@@ -17,26 +17,34 @@
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-#
+# 
 
 import numpy
 from gnuradio import gr
-from adsb_encoder import gen_ident
+from .adsb_encoder import gen_position
+import sys
 
-class adsb_ident_source(gr.sync_block):
+class adsb_position_source(gr.sync_block):
     """
-    docstring for block adsb_ident_source
+    docstring for block adsb_position_source
     """
-    def __init__(self, ec, icao, callsign):
+    def __init__(self,capability,icao,typecode,surveillancestatus,nicsupplementb,altitude,time,latitude,longitude,surface,intermessagegap):
         gr.sync_block.__init__(self,
-            name="adsb_ident_source",
+            name="adsb_position_source",
             in_sig=None,
             out_sig=[numpy.int8])
-        self.ec = ec
+        self.capability = capability
         self.icao = icao
-        self.callsign = callsign
-        self.data = gen_ident(self)
-
+        self.typecode = typecode
+        self.surveillancestatus = surveillancestatus
+        self.nicsupplementb = nicsupplementb
+        self.altitude = altitude
+        self.time = time
+        self.latitude = latitude
+        self.longitude = longitude
+        self.surface = surface
+        self.intermessagegap = intermessagegap
+        self.data = gen_position(self)
 
     def work(self, input_items, output_items):
         out = output_items[0]
@@ -45,6 +53,7 @@ class adsb_ident_source(gr.sync_block):
             out[:] = self.data[:out.shape[0]]
             self.data = self.data[out.shape[0]:]
         except IndexError:
-            self.data = self.data + gen_ident(self)
-        return len(output_items[0])
+            self.data = self.data + gen_position(self)
+
+        return len(out[:])
 
